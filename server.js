0161449c37e85c04d3edd9f69eab1823c5f3a29b
yuzhen <caiyuzhen00@gmail.com>
2022-10-24 14:54:53 +0800

    fix

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const port = 3005
 
 
 //配置 
-server.all('*',function (req, res, next) {
-    res.header('Access-Control-Allow-Origin','http://localhost:3000'); //当允许携带cookies此处的白名单不能写’*’
-    res.header('Access-Control-Allow-Headers','content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
-    res.header('Access-Control-Allow-Credentials',true);  //允许携带cookies
-    next();
-})
+// server.all('*',function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin','http://localhost:3000'); //当允许携带cookies此处的白名单不能写’*’
+//     res.header('Access-Control-Allow-Headers','content-type,Content-Length, Authorization,Origin,Accept,X-Requested-With'); //允许的请求头
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT'); //允许的请求方法
+//     res.header('Access-Control-Allow-Credentials',true);  //允许携带cookies
+//     next();
+// })
 
 
 // 获取 Notion 数据
--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	server: {
+		open: true,
 		proxy: { //代理配置以避免跨域问题
-			'/api':{
+			'/api': {
 				target: 'http://localhost:3005',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
-			},
+			},		
 		},
 	},
 })
