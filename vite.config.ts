edecfc7590141be749dab32b503d56fe770d5d73
yuzhen <caiyuzhen00@gmail.com>
2022-10-24 15:11:27 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		open: true,
+		cors: true, // 默认启用并允许任何源
+		port: 3005,
+		open: true, // 在服务器启动时自动在浏览器中打开应用程序
 		proxy: { //代理配置以避免跨域问题
 			'/api': {
-				target: 'http://localhost:3005',
+				target: 'http://localhost:3005',  //代理接口
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			},		
